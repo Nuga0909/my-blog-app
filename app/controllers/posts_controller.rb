217d0ec709f60595a75259c9854b1c3b9066d13b
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-07 21:04:24 +0100

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,13 @@
 class PostsController < ApplicationController
-    def index;
-        @user = User.find(params[:user_id])
-        @posts = @user.posts 
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-    end
-
-    def show; 
-        logger.debug(params)
-        @user = User.find(params[:user_id])
-        @post = @user.posts.find(params[:id])
-        @comments = @post.comments
-    end
+  def show
+    logger.debug(params)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
 end
