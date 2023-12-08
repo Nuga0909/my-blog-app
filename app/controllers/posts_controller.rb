07c6f709cdf52d078022b20ac5fd4c670ed0985b
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-08 14:30:57 +0100

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,34 @@
 class PostsController < ApplicationController
   def index
-        @user = User.find(params[:user_id])	      @user = User.find(params[:user_id])
-        @posts = @user.posts 	      @posts = @user.posts
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-    end
-    end	
+  def show
+    logger.debug(params)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
 
-    def show
-    def show; 	      logger.debug(params)
-        logger.debug(params)	      @user = User.find(params[:user_id])
-        @user = User.find(params[:user_id])	      @post = @user.posts.find(params[:id])
-        @post = @user.posts.find(params[:id])	      @comments = @post.comments
-        @comments = @post.comments	    end
-    end	
-end	    def new
-      @user = current_user
-      @post = Post.new
-    end
+  def new
+    @user = current_user
+    @post = Post.new
+  end
 
-    def create
-      @post = current_user.posts.build(post_params)
-      if @post.save
-        redirect_to user_posts_path, notice: 'Post was successfully created.'
-      else
-        flash.now[:alert] = 'Something Wrong, Cannot create a new post'
-        render :new
-      end
-    end
-
-    private
-
-    def post_params
-      params.require(:post).permit(:title, :text, :user_id)
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_posts_path, notice: 'Post was successfully created.'
+    else
+      flash.now[:alert] = 'Something Wrong, Cannot create a new post'
+      render :new
     end
   end
-  
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
+end
