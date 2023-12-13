c63ec0a6eb2beb2db00488cb95ff90c238e67136
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-13 11:28:22 +0100

    fix linter error

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -19,53 +19,19 @@ RSpec.feature 'Post Index Page' do
     create_comment(user1, post4, 'Next, Good job!')
   end
 
-  scenario 'Displays profile picture' do
+  scenario 'Displays user information and posts' do
     visit user_posts_path(user1)
+
     expect(page).to have_css("img[src='#{user1.photo}']")
-  end
-
-  scenario 'Displays username' do
-    visit user_posts_path(user1)
     expect(page).to have_content(user1.name)
-  end
-
-  scenario 'Displays posts count' do
-    visit user_posts_path(user1)
     expect(page).to have_content("Number Of posts #{user1.posts.count}")
-  end
-
-  scenario 'Displays post title' do
-    visit user_posts_path(user1)
     expect(page).to have_content(post1.title)
-  end
-
-  scenario 'Displays some part of post body' do
-    visit user_posts_path(user1)
     expect(page).to have_content(post1.text)
-  end
-
-  scenario 'Displays the 1st comment' do
-    visit user_posts_path(user1)
     expect(page).to have_content(comment1.body)
-  end
-
-  scenario 'Displays comments count' do
-    visit user_posts_path(user1)
     expect(page).to have_content("Comments #{post1.comments.count}")
-  end
-
-  scenario 'Displays likes count' do
-    visit user_posts_path(user1)
     expect(page).to have_content("Likes #{post1.likes.count}")
-  end
-
-  scenario 'Displays the pagination if more than 3 posts' do
-    visit user_posts_path(user1)
     expect(page).to have_content('Next')
-  end
 
-  scenario 'Redirects to post show page when clicked' do
-    visit user_posts_path(user1)
     click_link post1.title
     expect(current_path).to eq(user_post_path(user1, post1))
   end
@@ -73,10 +39,10 @@ RSpec.feature 'Post Index Page' do
   private
 
   def create_post(author, title, text)
-    Post.create(author_id: author.id, title:, text:, comments_counter: 0, likes_counter: 0)
+    Post.create(author_id: author.id, title: title, text: text, comments_counter: 0, likes_counter: 0)
   end
 
   def create_comment(author, post, body)
-    Comment.create(author:, post:, body:)
+    Comment.create(author: author, post: post, body: body)
   end
 end
