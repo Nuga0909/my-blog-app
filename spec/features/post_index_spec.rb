c27b4b1639ae4be77528367dc6e323d10910487a
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-13 11:18:02 +0100

    refactor post_index, post_show and user_show

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,70 +1,81 @@
 require 'rails_helper'
+
 RSpec.feature 'Post Index Page' do
-  let!(:user1) { User.create(name: 'Tom', bio: 'Teacher from Mexico.', photo: 'https://avatars.githubusercontent.com/u/98366229?v=4', posts_counter: 0) }
-  let!(:post1) do
-    Post.create(author_id: user1.id, title: 'Hello', text: 'This is my first post', comments_counter: 0,
-                likes_counter: 0)
+  let!(:user1) do
+    User.create(name: 'Tom', bio: 'Teacher from Mexico.', photo: 'https://avatars.githubusercontent.com/u/98366229?v=4', posts_counter: 0)
   end
-  let!(:post2) do
-    Post.create(author_id: user1.id, title: 'You are welcome', text: 'This is my second post', comments_counter: 0,
-                likes_counter: 0)
+
+  let!(:post1) { create_post(user1, 'Hello', 'This is my first post') }
+  let!(:post2) { create_post(user1, 'You are welcome', 'This is my second post') }
+  let!(:post3) { create_post(user1, 'Welcome to the new app', 'Tis is a welcome message to the new blog app') }
+  let!(:post4) { create_post(user1, 'this is new post', 'i like this') }
+
+  let!(:comment1) { create_comment(user1, post3, 'Perfect!') }
+
+  before do
+    create_comment(user1, post1, 'Good luck!')
+    create_comment(user1, post2, 'Never give up!')
+    create_comment(user1, post4, 'Next, Good job!')
   end
-  let!(:post3) do
-    Post.create(author_id: user1.id, title: 'Welcome to the new app',
-                text: 'Tis is a welcome message to the new blog app', comments_counter: 0, likes_counter: 0)
-  end
-  let!(:post4) do
-    Post.create(author_id: user1.id, title: 'this is new post', text: 'i like this', comments_counter: 0,
-                likes_counter: 0)
-  end
-  let!(:comment1) { Comment.create(author: user1, post: post3, body: 'Perfect!') }
-  let!(:comment2) { Comment.create(author: user1, post: post1, body: 'Good luck!') }
-  let!(:comment3) { Comment.create(author: user1, post: post2, body: 'Never give up!') }
-  let!(:comment4) { Comment.create(author: user1, post: post4, body: 'Next, Good job!') }
+
   scenario 'Displays profile picture' do
     visit user_posts_path(user1)
     expect(page).to have_css("img[src='#{user1.photo}']")
   end
+
   scenario 'Displays username' do
     visit user_posts_path(user1)
     expect(page).to have_content(user1.name)
   end
+
   scenario 'Displays posts count' do
     visit user_posts_path(user1)
     expect(page).to have_content("Number Of posts #{user1.posts.count}")
   end
+
   scenario 'Displays post title' do
     visit user_posts_path(user1)
     expect(page).to have_content(post1.title)
   end
+
   scenario 'Displays some part of post body' do
     visit user_posts_path(user1)
     expect(page).to have_content(post1.text)
   end
-  scenario 'Displays post title' do
-    visit user_posts_path(user1)
-    expect(page).to have_content(post1.title)
-  end
+
   scenario 'Displays the 1st comment' do
     visit user_posts_path(user1)
     expect(page).to have_content(comment1.body)
   end
+ 
   scenario 'Displays comments count' do
     visit user_posts_path(user1)
     expect(page).to have_content("Comments #{post1.comments.count}")
   end
+
   scenario 'Displays likes count' do
     visit user_posts_path(user1)
     expect(page).to have_content("Likes #{post1.likes.count}")
   end
+
   scenario 'Displays the pagination if more than 3 posts' do
     visit user_posts_path(user1)
     expect(page).to have_content('Next')
   end
+
   scenario 'Redirects to post show page when clicked' do
     visit user_posts_path(user1)
     click_link post1.title
-    sleep(5)
     expect(current_path).to eq(user_post_path(user1, post1))
+  end
+
+  private
+
+  def create_post(author, title, text)
+    Post.create(author_id: author.id, title: title, text: text, comments_counter: 0, likes_counter: 0)
+  end
+
+  def create_comment(author, post, body)
+    Comment.create(author: author, post: post, body: body)
   end
 end
--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,46 +1,45 @@
 require 'rails_helper'
+
 RSpec.feature 'User Show Page' do
-  let!(:user1) { User.create(name: 'Tom', bio: 'Teacher from Mexico.', photo: 'https://avatars.githubusercontent.com/u/98366229?v=4', posts_counter: 0) }
-  let!(:post1) { Post.create(author: user1, title: 'My first post') }
-  let!(:post2) { Post.create(author: user1, title: 'My second post') }
-  let!(:post3) { Post.create(author: user1, title: 'My third post') }
-  let!(:post4) { Post.create(author: user1, title: 'My forth post') }
-  scenario 'Displays profile pictures' do
+  let!(:user1) do
+    User.create(name: 'Tom', bio: 'Teacher from Mexico.', photo: 'https://avatars.githubusercontent.com/u/98366229?v=4', posts_counter: 0)
+  end
+
+  let!(:post1) { create_post(user1, 'My first post') }
+  let!(:post2) { create_post(user1, 'My second post') }
+  let!(:post3) { create_post(user1, 'My third post') }
+  let!(:post4) { create_post(user1, 'My forth post') }
+
+  scenario 'Displays user details' do
     visit user_path(user1)
+
     expect(page).to have_css("img[src='#{user1.photo}']")
-  end
-  scenario 'Displays usernames' do
-    visit user_path(user1)
     expect(page).to have_content(user1.name)
-  end
-  scenario 'Displays post counts' do
-    visit user_path(user1)
     expect(page).to have_content("Number Of posts #{user1.posts.count}")
-  end
-  scenario 'Displays user bio' do
-    visit user_path(user1)
     expect(page).to have_content(user1.bio)
-  end
-  scenario 'Displays the first 3 posts' do
-    visit user_path(user1)
-    user1.posts.limit(3).each do |_post|
-      expect(page).to have_content(post1.title)
+
+    user1.posts.limit(3).each do |post|
+      expect(page).to have_content(post.title)
     end
-  end
-  scenario 'Displays the button to see all posts' do
-    visit user_path(user1)
+
     expect(page).to have_content('View All Posts')
   end
-  scenario 'Redirects to post show page when clicked' do
+
+  scenario 'Redirects to post show page when post title clicked' do
     visit user_path(user1)
     click_link post1.title
-    sleep(5)
     expect(current_path).to eq(user_post_path(user1, post1))
   end
-  scenario 'Redirects to post index page when clicked' do
+
+  scenario 'Redirects to post index page when "View All Posts" clicked' do
     visit user_path(user1)
     click_link 'View All Posts'
-    sleep(5)
     expect(current_path).to eq(user_posts_path(user1))
+  end
+
+  private
+
+  def create_post(author, title)
+    Post.create(author: author, title: title)
   end
 end
