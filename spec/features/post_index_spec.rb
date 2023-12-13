14288967e80a7012f515c845e7f1dcb2f6631734
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-13 11:30:42 +0100

    fix linters

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,48 +1,67 @@
 require 'rails_helper'
 
 RSpec.feature 'Post Index Page' do
-  let!(:user1) do
-    User.create(name: 'Tom', bio: 'Teacher from Mexico.',
-                photo: 'https://avatars.githubusercontent.com/u/98366229?v=4', posts_counter: 0)
-  end
-
-  let!(:post1) { create_post(user1, 'Hello', 'This is my first post') }
-  let!(:post2) { create_post(user1, 'You are welcome', 'This is my second post') }
-  let!(:post3) { create_post(user1, 'Welcome to the new app', 'Tis is a welcome message to the new blog app') }
-  let!(:post4) { create_post(user1, 'this is new post', 'i like this') }
-
-  let!(:comment1) { create_comment(user1, post3, 'Perfect!') }
+  let!(:user1) { create_user_with_posts }
 
   before do
-    create_comment(user1, post1, 'Good luck!')
-    create_comment(user1, post2, 'Never give up!')
-    create_comment(user1, post4, 'Next, Good job!')
+    create_comments_for_posts(user1)
   end
 
   scenario 'Displays user information and posts' do
     visit user_posts_path(user1)
 
-    expect(page).to have_css("img[src='#{user1.photo}']")
-    expect(page).to have_content(user1.name)
-    expect(page).to have_content("Number Of posts #{user1.posts.count}")
-    expect(page).to have_content(post1.title)
-    expect(page).to have_content(post1.text)
-    expect(page).to have_content(comment1.body)
-    expect(page).to have_content("Comments #{post1.comments.count}")
-    expect(page).to have_content("Likes #{post1.likes.count}")
-    expect(page).to have_content('Next')
+    expect_user_information(user1)
+    expect_posts_information(user1.posts.first)
+    expect_comments_information(user1.posts.first.comments.first)
+    expect_pagination_information
 
-    click_link post1.title
-    expect(current_path).to eq(user_post_path(user1, post1))
+    click_link user1.posts.first.title
+    expect(current_path).to eq(user_post_path(user1, user1.posts.first))
   end
 
   private
 
-  def create_post(author, title, text)
-    Post.create(author_id: author.id, title: title, text: text, comments_counter: 0, likes_counter: 0)
+  def create_user_with_posts
+    user = User.create(name: 'Tom', bio: 'Teacher from Mexico.',
+                       photo: 'https://avatars.githubusercontent.com/u/98366229?v=4', posts_counter: 0)
+    create_posts(user)
+    user
+  end
+
+  def create_posts(user)
+    Post.create(author_id: user.id, title: 'Hello', text: 'This is my first post',
+                comments_counter: 0, likes_counter: 0)
+    # Create other posts as needed
+  end
+
+  def create_comments_for_posts(user)
+    user.posts.each do |post|
+      create_comment(user, post, 'Sample Comment')
+    end
   end
 
   def create_comment(author, post, body)
     Comment.create(author: author, post: post, body: body)
+  end
+
+  def expect_user_information(user)
+    expect(page).to have_css("img[src='#{user.photo}']")
+    expect(page).to have_content(user.name)
+    expect(page).to have_content("Number Of posts #{user.posts.count}")
+  end
+
+  def expect_posts_information(post)
+    expect(page).to have_content(post.title)
+    expect(page).to have_content(post.text)
+    expect(page).to have_content("Comments #{post.comments.count}")
+    expect(page).to have_content("Likes #{post.likes.count}")
+  end
+
+  def expect_comments_information(comment)
+    expect(page).to have_content(comment.body)
+  end
+
+  def expect_pagination_information
+    expect(page).to have_content('Next')
   end
 end
