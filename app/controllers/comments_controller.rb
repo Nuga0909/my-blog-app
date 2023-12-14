a173cb9238e8544ec715d834a68d11a7243adcff
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-14 12:11:07 +0100

    update comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,24 @@
 class CommentsController < ApplicationController
   def new
-    @user = Post.includes(:user).find(params[:post_id]).user
     @post = Post.find(params[:post_id])
+    @user = @post.author
     @comment = Comment.new
   end
 
   def create
     @user = current_user
+    @post = Post.find(params[:post_id])
     @comment = @user.comments.build(comment_params)
-    @comment.post_id = params[:post_id]
+    @comment.post = @post
 
     if @comment.save
       flash[:success] = 'Comment created successfully'
+      redirect_to user_post_path(params[:user_id], @comment.post_id)
     else
       flash[:error] = 'Something went wrong.'
       puts @comment.errors.full_messages
       render :new
     end
-
-    redirect_to user_post_path(params[:user_id], @comment.post_id)
   end
 
   private
