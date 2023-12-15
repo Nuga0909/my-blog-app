b5748baee803a815eb2185cd1d18fef1a023c17a
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-15 09:04:52 +0100

    adjust user controller for html and json requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { user: @user, posts: @posts } }
+    end
   end
 end
