cce62c6bc354093def1efa94509bbcf438539cf5
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-15 09:02:47 +0100

    adjust poats controller for html and json requests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,44 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_user, only: [:index, :show, :new, :create]
+
   def index
-    @user = User.includes(posts: :comments).find(params[:user_id])
     @posts = @user.posts
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 
   def show
-    logger.debug(params)
-    @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
     @comments = @post.comments
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { post: @post, comments: @comments } }
+    end
   end
 
   def new
-    @user = current_user
     @post = Post.new
   end
 
   def create
-    @post = current_user.posts.build(post_params)
-    if @post.save
-      redirect_to user_posts_path, notice: 'Post was successfully created.'
-    else
-      flash.now[:alert] = 'Something Wrong, Cannot create a new post'
-      render :new
+    @post = @user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_posts_path, notice: 'Post was successfully created.' }
+        format.json { render json: @post, status: :created }
+      else
+        format.html do
+          flash.now[:alert] = 'Something Wrong, Cannot create a new post'
+          render :new
+        end
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -31,5 +46,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :text, :user_id)
+  end
+
+  def set_user
+    @user = if params[:user_id]
+              User.find(params[:user_id])
+            else
+              current_user
+            end
   end
 end
