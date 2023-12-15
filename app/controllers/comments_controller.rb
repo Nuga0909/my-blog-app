a16417db47ca84c01a5576812844781bb60dfc26
Adeshina Adenuga <Nuga0909@gmail.com>
2023-12-15 08:59:18 +0100

    adjust commentscontroller for html and json requests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,39 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:new, :create, :index]
+  before_action :set_user, only: [:new, :create, :index]
+
   def new
-    @post = Post.find(params[:post_id])
-    @user = @post.author
     @comment = Comment.new
   end
 
   def create
-    @user = current_user
-    @post = Post.find(params[:post_id])
     @comment = @user.comments.build(comment_params)
     @comment.post = @post
 
-    if @comment.save
-      flash[:success] = 'Comment created successfully'
-      redirect_to user_post_path(params[:user_id], @comment.post_id)
-    else
-      flash[:error] = 'Something went wrong.'
-      puts @comment.errors.full_messages
-      render :new
+    respond_to do |format|
+      if @comment.save
+        format.html do
+          flash[:success] = 'Comment created successfully'
+          redirect_to user_post_path(params[:user_id], @comment.post_id)
+        end
+        format.json { render json: @comment, status: :created }
+      else
+        format.html do
+          flash[:error] = 'Something went wrong.'
+          puts @comment.errors.full_messages
+          render :new
+        end
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # API endpoint to list all comments for a user's post
+  def index
+    @comments = @post.comments
+    respond_to do |format|
+      format.html
+      format.json { render json: @comments }
     end
   end
 
@@ -25,5 +41,13 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_user
+    @user = @post.author
   end
 end
